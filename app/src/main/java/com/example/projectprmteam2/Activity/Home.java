43a5e1d52574a4e2c096f28013123b435e78ee6f
Kyle - DA <inuyashapc162@gmail.com>
2023-10-30 16:38:48 +0700

    'feat:fragment'

--- app/src/main/java/com/example/projectprmteam2/Activity/Home.java
+++ app/src/main/java/com/example/projectprmteam2/Activity/Home.java
@@ -1,22 +1,39 @@
 package com.example.projectprmteam2.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.projectprmteam2.Fragment.Fragment_Home;
 import com.example.projectprmteam2.R;
 import com.example.projectprmteam2.Sharepreference;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class Home extends AppCompatActivity {
     Sharepreference sharepreference;
+    private BottomNavigationView bottomNavigationView;
+    private Fragment fragment;
+    private FragmentManager manager;
+    private FragmentTransaction transaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.home);
         sharepreference=new Sharepreference(this);
+        Init();
+
+
 //        Button btnlogout= findViewById(R.id.button);
 //
 //        btnlogout.setOnClickListener(new View.OnClickListener() {
@@ -28,5 +45,40 @@ public class Home extends AppCompatActivity {
 //                finish();
 //            }
 //        });
+    }
+
+    private void Init() {
+        fragment = new Fragment_Home();
+        manager = getSupportFragmentManager();
+        transaction =manager.beginTransaction();
+        bottomNavigationView.setBackground(null);
+        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,fragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.home) {
+                    fragment = new Fragment_Home();
+                }
+
+//                switch (item.getItemId()){
+//                    case  R.id.home:
+//                        fragment = new Fragment_Home();
+//                        break;
+//                    case  R.id.donhang:
+//                        fragment=new FragMent_Bill();
+//                        break;
+//                    case  R.id.person:
+//                        fragment = new SettingFragment();
+//                        break;
+//                    case  R.id.category:
+//                        startActivity(new Intent(HomeActivity.this, ThongKeDanhMucActivity.class));break;
+//                }
+                if(fragment!=null){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,fragment).commit();
+                }
+                //drawerLayout.closeDrawers();
+                return true;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/projectprmteam2/Fragment/Fragment_Home.java
@@ -0,0 +1,26 @@
+package com.example.projectprmteam2.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.projectprmteam2.R;
+
+public class Fragment_Home extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
